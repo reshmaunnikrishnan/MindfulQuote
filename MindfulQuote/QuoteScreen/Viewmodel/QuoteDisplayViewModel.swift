236c28a61fd8846fd5d19b28ec86b89fbdc0bd7d
Reshma Unnikrishnan <ruvlmoon@gmail.com>
2022-09-20 20:22:59 +0200

    Create viewmodel that wraps the UI Related  data, used protocols to create  the needed abstarction expected in viewmodel

---
+++ MindfulQuote/QuoteScreen/Viewmodel/QuoteDisplayViewModel.swift
@@ -0,0 +1,58 @@
+//
+//  MindfulQuotesViewModel.swift
+//  MindfulQuote
+//
+//  Created by Reshma Unnikrishnan on 20.09.22.
+//
+
+import Foundation
+
+protocol QuoteDisplayViewModelInput {
+    func fetchQuote(lang: String, completion: @escaping () -> Void)
+    var showAlert: (() -> Void)? { get set }
+    var showErrorAlert: (() -> Void)? { get set }
+    var quote: Quote? { get set }
+    var error: Error? { get set }
+}
+
+final class QuoteDisplayViewModel: QuoteDisplayViewModelInput {
+    
+    // MARK: - Properties
+    var showAlert: (() -> Void)?
+    var showErrorAlert: (() -> Void)?
+    
+    var error: Error? {
+        didSet { showErrorAlert?() }
+    }
+    
+    var quote: Quote? {
+        didSet {
+            showAlert?()
+        }
+    }
+    
+    // MARK: - Initialization
+    let dataRequest: DataRequestProtocol
+    init(dataRequest: DataRequestProtocol) {
+        self.dataRequest = dataRequest
+    }
+
+    // MARK: - Public Methods
+    func fetchQuote(lang: String, completion: @escaping () -> Void)  {
+        dataRequest.sendRequest(endPoint: Endpoint.fetchSearchGiphy(with: lang), type: Quote.self) { [weak self] result in
+            switch result {
+            case .failure(let error):
+                self?.error = error
+                completion()
+                print(error)
+            case .success(let quote):
+                print(quote)
+                self?.quote = quote
+                completion()
+
+            }
+        }
+    }
+
+}
+
