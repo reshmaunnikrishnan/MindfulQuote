b422238f218e7111f9507c0f9a370eba4f94451e
Reshma Unnikrishnan <ruvlmoon@gmail.com>
2022-09-20 09:47:30 +0200

    Initial Commit

---
+++ MindfulQuote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MindfulQuote
+//
+//  Created by Reshma Unnikrishnan on 20.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
